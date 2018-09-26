0511c2cc71a55ca837e0a7ecc96197b7411ae987
Garin87 <kamrader@gmail.com>
2018-09-26 09:42:23 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-module.exports = function getZerosCount(number) {
-  // your implementation
-}
+module.exports = function getZerosCount(a) {
+      
+var count = 0; // counter of zeros
+var div = 1; // result of division
+var n = 1; // degree of
+
+	while(div>=1){
+	   count = count + Math.floor(a / Math.pow(5, n));
+	   div = Math.floor(a / Math.pow(5, n));
+	   n++;  
+	}
+
+	return count;
+} 
